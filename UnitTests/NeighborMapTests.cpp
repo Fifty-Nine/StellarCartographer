fbfbee99dacd49d743db735dc9c5553d42700070
Fifty-Nine <tim.prince@gmail.com>
2014-09-14 01:31:25 -0700

    Add test stubs

---
+++ UnitTests/NeighborMapTests.cpp
@@ -0,0 +1,10 @@
+#include "UnitTests/Tests.h"
+
+#include <cmath>
+#include "StellarCartography/NeighborMap.h"
+
+using namespace StellarCartography;
+
+SC_TEST_SUITE(NeighborMapTests)
+
+SC_TEST_SUITE_END()
---
+++ UnitTests/SpatialIndexTests.cpp
@@ -0,0 +1,10 @@
+#include "UnitTests/Tests.h"
+
+#include <cmath>
+#include "StellarCartography/SpatialIndex.h"
+
+using namespace StellarCartography;
+
+SC_TEST_SUITE(SpatialIndexTests)
+
+SC_TEST_SUITE_END()
