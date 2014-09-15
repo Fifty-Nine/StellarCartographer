084ed81872999e195c670333f53081425ff9955f
Fifty-Nine <tim.prince@gmail.com>
2014-09-14 18:53:11 -0700

    Add readline command completion

--- Query/main.cpp
+++ Query/main.cpp
@@ -40,7 +40,88 @@ SpatialIndex loadIndex()
 typedef std::vector<std::string> ArgList;
 typedef std::function<void (ArgList)> QueryFcn;
 typedef std::unordered_map<std::string, QueryFcn> CommandTable;
+using namespace std; 
+using namespace boost;
 
+SpatialIndex g = loadIndex();
+
+CommandTable cmds 
+{
+    { 
+        "nearest", 
+        [](ArgList args) 
+        {
+            Star from = g.getStar(args[1]);
+            Star to = 
+                g.nearestNeighbor(from, lexical_cast<double>(args[2]));
+            cout << "Neighbor: " << to.getName() 
+                 << " Distance: " 
+                 << from.getCoords().distance(to.getCoords()) 
+                 << endl;
+        }
+    },
+    { 
+        "neighbors", 
+        [](ArgList args)
+        {
+            Star from = g.getStar(args[1]);
+            double t = lexical_cast<double>(args[2]);
+            for (auto n : g.neighbors(from, t))
+            {
+                cout << "Neighbor: " << n.getName()
+                     << " Distance: " 
+                     << from.getCoords().distance(n.getCoords()) 
+                     << endl;
+            }
+        }
+    },
+    {
+        "path",
+        [](ArgList a)
+        {
+            Star from = g.getStar(a[1]);
+            Star to = g.getStar(a[2]);
+            double t = lexical_cast<double>(a[3]);
+
+            for (auto u : g.path(from, to, t))
+            {
+                cout << u.getName() << " " 
+                     << from.getCoords().distance(u.getCoords())
+                     << endl;
+                from = u;
+            }
+        }
+    },
+    {
+        "reachable",
+        [](ArgList a)
+        {
+            Star from = g.getStar(a[1]);
+            double t = lexical_cast<double>(a[2]);
+
+            for (auto v : g.reachable(from, t))
+            {
+                cout << v.getName() << std::endl;
+            }
+        }
+    },
+    {
+        "connected",
+        [](ArgList a)
+        {
+            double t = lexical_cast<double>(a[1]);
+
+            for (auto cc : g.connectedComponents(t))
+            {
+                cout << "Component:" << endl;
+                for (auto v : cc)
+                {
+                    cout << "\t" << v.getName() << endl;
+                }
+            }
+        }
+    }
+};
 
 void processQuery(const CommandTable& c, const std::string& cmd)
 {
@@ -70,9 +151,47 @@ void processQuery(const CommandTable& c, const std::string& cmd)
     }
 }
 
+std::vector<std::string> cmd_completions(const std::string& text)
+{
+    std::vector<std::string> candidates;
+    for (auto c : cmds)
+    {
+        if (boost::starts_with(c.first, text))
+        {
+            candidates.push_back(c.first);
+        }
+    }
+    return candidates;
+}
+
+char **completer(const char *text, int start, int end)
+{
+    if (start != 0)
+    {
+        rl_bind_key('\t', rl_abort);
+        return NULL;   
+    }
+
+    auto candidates = cmd_completions(text);
+    if (candidates.empty()) { return NULL; }
+
+    char **result = (char**)malloc(sizeof(char*) * (candidates.size() + 1));
+
+    for (size_t i = 0; i < candidates.size(); ++i)
+    {
+        result[i] = strdup(candidates[i].c_str());
+    }
+    result[candidates.size()] = 0;
+    return result;
+}
+
 bool prompt(std::string& s)
 {
     using namespace std;
+
+    rl_attempted_completion_function = &completer;
+    rl_bind_key('\t', rl_complete);
+
     char *buf = readline("scq $ ");
 
     if (!buf) return false;
@@ -88,85 +207,6 @@ int main(int argc, char *argv[])
 {
     using namespace std;
     using namespace boost;
-    SpatialIndex g = loadIndex();
-
-    CommandTable cmds 
-    {
-        { 
-            "nearest", 
-            [&g](ArgList args) 
-            {
-                Star from = g.getStar(args[1]);
-                Star to = 
-                    g.nearestNeighbor(from, lexical_cast<double>(args[2]));
-                cout << "Neighbor: " << to.getName() 
-                     << " Distance: " 
-                     << from.getCoords().distance(to.getCoords()) 
-                     << endl;
-            }
-        },
-        { 
-            "neighbors", 
-            [&g](ArgList args)
-            {
-                Star from = g.getStar(args[1]);
-                double t = lexical_cast<double>(args[2]);
-                for (auto n : g.neighbors(from, t))
-                {
-                    cout << "Neighbor: " << n.getName()
-                         << " Distance: " 
-                         << from.getCoords().distance(n.getCoords()) 
-                         << endl;
-                }
-            }
-        },
-        {
-            "path",
-            [&g](ArgList a)
-            {
-                Star from = g.getStar(a[1]);
-                Star to = g.getStar(a[2]);
-                double t = lexical_cast<double>(a[3]);
-
-                for (auto u : g.path(from, to, t))
-                {
-                    cout << u.getName() << " " 
-                         << from.getCoords().distance(u.getCoords())
-                         << endl;
-                    from = u;
-                }
-            }
-        },
-        {
-            "reachable",
-            [&g](ArgList a)
-            {
-                Star from = g.getStar(a[1]);
-                double t = lexical_cast<double>(a[2]);
-
-                for (auto v : g.reachable(from, t))
-                {
-                    cout << v.getName() << std::endl;
-                }
-            }
-        },
-        {
-            "connected",
-            [&g](ArgList a)
-            {
-                double t = lexical_cast<double>(a[1]);
-
-                for (auto cc : g.connectedComponents(t))
-                {
-                    cout << "Component:" << endl;
-                    for (auto v : cc)
-                    {
-                        cout << "\t" << v.getName() << endl;
-                    }
-                }
-            }
-        }
-    };
 
     while (true)
     {
